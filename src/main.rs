a784251ae8c4bb5bd1f1ee01573a7dc40199cd84
cooksey <suitcase24@gmail.com>
2018-06-14 09:58:18 -0400

    Test appending string in loop

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,17 @@ const BUF_SIZE: i32 = 1024;
 
 fn main() -> std::io::Result<()> {
     
-    let f = File::open("./resources/test.tx").expect("Could not find the file!");
-    let mut reader = BufReader::new(f);
+    let f = File::open("./resources/test.txt").expect("Could not find the file!");
+    let reader = BufReader::new(f);
     
+    let buf_counter = 0;
+    let mut buf = String::new();
     for line in reader.lines() {
+        if (buf_counter <= BUF_SIZE) {
+            
+        } else {
+
+        }
         let line = line?;
         println!("line {}", line);
     }
